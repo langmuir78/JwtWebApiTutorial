120e3b83b035ca88f719cbf971c24ac9b244d07a
Giuliano Sanasi D'Ammando <giuliano.dammando@mail.polimi.it>
2023-07-20 19:48:45 +0200

    Initial commit

---
+++ JwtWebApiTutorial/User.cs
@@ -0,0 +1,11 @@
+namespace JwtWebApiTutorial
+{
+  public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; } = new byte[0];
+
+        public byte[] PasswordSalt { get; set; } = new byte[0];
+    }
+}
